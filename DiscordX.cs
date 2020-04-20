4a8bfe0fe155dc25140a7ba1659b0ed32f9f2b3e
HoldMyBeerInc <64022628+HoldMyBeerInc@users.noreply.github.com>
2020-04-20 15:28:20 +0100

    Create DiscordX.cs

---
+++ DiscordX.cs
@@ -0,0 +1,41 @@
+//META{"name":"Discord X","description":"A fully customizable and cool theme!","author":"HoldMyBeer#0001","version":"0.0.1","source":"https://github.com/codedotspectra/themes","website":"https://codedotspectra.github.io/me/"}*//{}
+
+@import url("https://github.com/HoldMyBeerInc/DiscordThemes/blob/master/ThemeCore.css");
+/*###################################################################################################*/
+:root {
+    /* Images */
+    --HomeButtonImg:url("https://arudarin.github.io/themes/neutron/import/homeBtn.png");
+    --brightness: 0.4;
+
+    /* font */
+    --customFont: 'Montserrat';
+
+    /* Colors */
+    --PrimaryColor01: rgb(32, 197, 119); /* Theme's color 01 */
+    --PrimaryColorRGBA: rgba(32, 197, 119, 0.15); /* Theme's color 01 (transparent) */
+    --PrimaryColor02: #07a8c5; /* Theme's color 02 */
+    --DangerColor: #dc3545; /* Color showing a danger or an important action */
+    --WarningColor: #ffc107; /* Color for stuff you should look for */
+    --SuccesColor: #28a745; /* Color showing a succesful action */
+    --InfoColor: #17a2b8; /* Color showing an information or basic action */
+    --DangerColor02: #ad1f2d; /* Color showing a danger or an important action (darker) */
+    --WarningColor02: #cc9900; /* Color for stuff you should look for (darker) */
+    --SuccesColor02: #1e7b34; /* Color showing a succesful action (darker) */
+    --InfoColor02: #117888; /* Color showing an information or basic action (darker)*/
+    --DefaultColor: #6c757d; /* Default color */
+    --DefaultColor02: #5a6268;/* Default color (darker)*/
+
+    /* Texts colors */
+    --ChatTextColor: hsla(0, 100%, 100%, 1);
+    --RoleTitleColor: hsla(0, 100%, 100%, .95);
+    --UserPlayingTextColor: hsla(0, 100%, 100%, .6);
+    --ChannelsSelectedTextColor: hsla(0, 100%, 100%, .95);
+    --ChannelsDefaultTextColor: hsla(0, 100%, 100%, .6);
+    --ChannelsDarkTextColor: hsla(0, 100%, 100%, .2);
+
+    /* background colors (non-trasnparent)*/
+    --NTBackgroundColor01: #3a3f4b; /* Background color (bright) */
+    --NTBackgroundColor02: #2b3039; /* Background color (less bright) */
+    --NTBackgroundColor03: #282c34; /* Background color (dark) */
+    --NTBackgroundColor04: #21252c; /* Background color (super dark) */
+}
